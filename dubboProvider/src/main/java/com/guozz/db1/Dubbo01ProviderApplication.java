952dbda0c434a7afa667550e43e6ccf6ce14bca5
guozhizhong <guo_zhizhong@sina.com>
2021-07-28 11:06:30 +0800

    增加dubbo实例

---
+++ dubboProvider/src/main/java/com/guozz/db1/Dubbo01ProviderApplication.java
@@ -0,0 +1,14 @@
+package com.guozz.db1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class Dubbo01ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dubbo01ProviderApplication.class, args);
+	}
+
+}
