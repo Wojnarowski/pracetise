1839eafc1858ba85c10917f53e6c22d57f5594e6
guozhizhong <guo_zhizhong@sina.com>
2021-09-06 17:31:17 +0800

    新增

---
+++ netty/src/main/java/com/guozz/netty/sample/NettyServer.java
@@ -0,0 +1,74 @@
+package com.guozz.netty.sample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * @ClassName NettyServer
+ * @Description TODO
+ * @Author paul
+ * @Date 2021/9/5 20:19
+ * Vertion 1.0
+ * -------------------------------------------------------------_ooOoo_
+ * ------------------------------------------------------------o8888888o
+ * ------------------------------------------------------------88"-.-"88
+ * ------------------------------------------------------------(|--_--|)
+ * ------------------------------------------------------------O\--=--/O
+ * ---------------------------------------------------------____/`---'\____
+ * -------------------------------------------------------.'--\\|-----|//--`.
+ * ------------------------------------------------------/--\\|||--:--|||//--\
+ * -----------------------------------------------------/--_|||||--:--|||||---\
+ * -----------------------------------------------------|---|-\\\-----///-|---|
+ * -----------------------------------------------------|-\_|--''\---/''--|---|
+ * -----------------------------------------------------\--.-\__--`-`--___/-.-/
+ * ---------------------------------------------------___`.-.'--/--.--\--`.-.-__
+ * ------------------------------------------------.""-'<--`.___\_<|>_/___.'-->'"".
+ * -----------------------------------------------|-|-:--`--\`.;`\-_-/`;.`/---`-:-|-|
+ * -----------------------------------------------\--\-`-.---\_-__\-/__-_/---.-`-/--/
+ * ---------------------------------------======`-.____`-.___\_____/___.-`____.-'======
+ * -------------------------------------------------------------`=---='
+ * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * -----------------------------------------------------佛祖保佑--------永无BUG
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+
+        //创建BossGroup 和 WorkerGroup
+        //说明
+        //1. 创建两个线程组 bossGroup 和 workerGroup
+        //2. bossGroup 只是处理连接请求 , 真正的和客户端业务处理，会交给 workerGroup完成
+        //3. 两个都是无限循环
+        //4. bossGroup 和 workerGroup 含有的子线程(NioEventLoop)的个数
+        //   默认实际 cpu核数 * 2
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //8
+
+        try {
+            //创建服务器端的启动对象，配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            //使用链式编程来进行设置
+            bootstrap.group(bossGroup, workerGroup) //设置两个线程组
+                    .channel(NioServerSocketChannel.class) //使用NioSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128) // 设置线程队列得到连接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) //设置保持活动连接状态
+                    //          .handler(null) // 该 handler对应 bossGroup , childHandler 对应 workerGroup
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//创建一个通道初始化对象(匿名对象)
+                        //给pipeline 设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            System.out.println("客户socketchannel hashcode=" + ch.hashCode()); //可以使用一个集合管理 SocketChannel， 再推送消息时，可以将业务加入到各个channel 对应的 NIOEventLoop 的 taskQueue 或者 scheduleTaskQueue
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    }); // 给我们的workerGroup 的 EventLoop 对应的管道设置处理器
+
+            System.out.println(".....服务器 is ready...");
+    }
+}
+
