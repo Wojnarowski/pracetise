2c0c11764262e098548be40bc98372b97a0aa760
guozhizhong <guo_zhizhong@sina.com>
2021-09-06 17:57:52 +0800

    新增

--- netty/src/main/java/com/guozz/netty/sample/NettyClientHandler.java
+++ netty/src/main/java/com/guozz/netty/sample/NettyClientHandler.java
@@ -1,18 +1,16 @@
 package com.guozz.netty.sample;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 /**
- * @ClassName NettyClient
+ * @ClassName NettyClientHandler
  * @Description TODO
  * @Author paul
- * @Date 2021/9/6 17:03
+ * @Date 2021/9/6 17:51
  * Vertion 1.0
  * -------------------------------------------------------------_ooOoo_
  * ------------------------------------------------------------o8888888o
@@ -35,40 +33,28 @@ import io.netty.channel.socket.nio.NioSocketChannel;
  * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  * -----------------------------------------------------佛祖保佑--------永无BUG
  */
-public class NettyClient {
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
-    public static void main(String[] args) {
-        //客户端需要一个事件循环组
-        EventLoopGroup group = new NioEventLoopGroup();
-        try{
-
-            //创建客户端启动对象
-            //注意客户端使用的不是 ServerBootstrap 而是 Bootstrap
-            Bootstrap bootstrap = new Bootstrap();
-            //设置相关参数
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new NettyClientHandler()); //加入自己的处理器
-                        }
-                    });
-
-            System.out.println("客户端 ok..");
-            //启动客户端去连接服务器端
-            //关于 ChannelFuture 要分析，涉及到netty的异步模型
-            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
-            //给关闭通道进行监听
-            channelFuture.channel().closeFuture().sync();
-
-        }catch (Exception e){
-
-        }finally {
-            group.shutdownGracefully();
-        }
-
-
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client " + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello, server: (>^ω^<)喵", CharsetUtil.UTF_8));
     }
+
+    //当通道有读取事件时，会触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址： " + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
 }
 
--- netty/src/main/java/com/guozz/netty/sample/NettyServer.java
+++ netty/src/main/java/com/guozz/netty/sample/NettyServer.java
@@ -1,9 +1,7 @@
 package com.guozz.netty.sample;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -38,7 +36,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  */
 public class NettyServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         //创建BossGroup 和 WorkerGroup
         //说明
@@ -69,6 +67,33 @@ public class NettyServer {
                     }); // 给我们的workerGroup 的 EventLoop 对应的管道设置处理器
 
             System.out.println(".....服务器 is ready...");
+
+            //绑定一个端口并且同步, 生成了一个 ChannelFuture 对象
+            //启动服务器(并绑定端口)
+            final ChannelFuture cf = bootstrap.bind(6668).sync();
+
+            //给cf 注册监听器，监控我们关心的事件
+
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口 6668 成功");
+                    } else {
+                        System.out.println("监听端口 6668 失败");
+                    }
+                }
+            });
+
+            //对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
     }
 }
 
