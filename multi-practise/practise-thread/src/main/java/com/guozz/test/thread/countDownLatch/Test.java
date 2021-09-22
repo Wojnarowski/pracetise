0da5ea10f447f755f8b56132c340445f028feee3
guozhizhong <guo_zhizhong@sina.com>
2021-09-22 22:56:25 +0800

    新增

---
+++ multi-practise/practise-thread/src/main/java/com/guozz/test/thread/countDownLatch/Test.java
@@ -0,0 +1,72 @@
+package com.guozz.test.thread.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author paul
+ * @Date 2021/9/22 22:53
+ * Vertion 1.0
+ * -------------------------------------------------------------_ooOoo_
+ * ------------------------------------------------------------o8888888o
+ * ------------------------------------------------------------88"-.-"88
+ * ------------------------------------------------------------(|--_--|)
+ * ------------------------------------------------------------O\--=--/O
+ * ---------------------------------------------------------____/`---'\____
+ * -------------------------------------------------------.'--\\|-----|//--`.
+ * ------------------------------------------------------/--\\|||--:--|||//--\
+ * -----------------------------------------------------/--_|||||--:--|||||---\
+ * -----------------------------------------------------|---|-\\\-----///-|---|
+ * -----------------------------------------------------|-\_|--''\---/''--|---|
+ * -----------------------------------------------------\--.-\__--`-`--___/-.-/
+ * ---------------------------------------------------___`.-.'--/--.--\--`.-.-__
+ * ------------------------------------------------.""-'<--`.___\_<|>_/___.'-->'"".
+ * -----------------------------------------------|-|-:--`--\`.;`\-_-/`;.`/---`-:-|-|
+ * -----------------------------------------------\--\-`-.---\_-__\-/__-_/---.-`-/--/
+ * ---------------------------------------======`-.____`-.___\_____/___.-`____.-'======
+ * -------------------------------------------------------------`=---='
+ * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * -----------------------------------------------------佛祖保佑--------永无BUG
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        final CountDownLatch cdOrder = new CountDownLatch(1);
+        final CountDownLatch cdAnswer = new CountDownLatch(4);
+        for (int i = 0; i < 4; i++) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("选手" + Thread.currentThread().getName() + "正在等待裁判发布口令");
+                        cdOrder.await();
+                        System.out.println("选手" + Thread.currentThread().getName() + "已接受裁判口令");
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("选手" + Thread.currentThread().getName() + "到达终点");
+                        cdAnswer.countDown();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.execute(runnable);
+        }
+        try {
+            Thread.sleep((long) (Math.random() * 10000));
+            System.out.println("裁判"+Thread.currentThread().getName()+"即将发布口令");
+            cdOrder.countDown();
+            System.out.println("裁判"+Thread.currentThread().getName()+"已发送口令，正在等待所有选手到达终点");
+            cdAnswer.await();
+            System.out.println("所有选手都到达终点");
+            System.out.println("裁判"+Thread.currentThread().getName()+"汇总成绩排名");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+}
+
