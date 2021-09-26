2d1e9c54474088c3b27344ea4e810f23f924095f
guozhizhong <guo_zhizhong@sina.com>
2021-09-26 18:46:51 +0800

    新增

---
+++ netty/src/main/java/com/guozz/netty/chapter6/ChannelInboundHandlerAdapter.java
@@ -0,0 +1,79 @@
+package com.guozz.netty.chapter6;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+
+/**
+ * @ClassName ChannelInboundHandlerAdapter
+ * @Description TODO
+ * @Author paul
+ * @Date 2021/9/26 18:44
+ * Vertion 1.0
+ * -------------------------------------------------------------_ooOoo_
+ * ------------------------------------------------------------o8888888o
+ * ------------------------------------------------------------88"-.-"88
+ * ------------------------------------------------------------(|--_--|)
+ * ------------------------------------------------------------O\--=--/O
+ * ---------------------------------------------------------____/`---'\____
+ * -------------------------------------------------------.'--\\|-----|//--`.
+ * ------------------------------------------------------/--\\|||--:--|||//--\
+ * -----------------------------------------------------/--_|||||--:--|||||---\
+ * -----------------------------------------------------|---|-\\\-----///-|---|
+ * -----------------------------------------------------|-\_|--''\---/''--|---|
+ * -----------------------------------------------------\--.-\__--`-`--___/-.-/
+ * ---------------------------------------------------___`.-.'--/--.--\--`.-.-__
+ * ------------------------------------------------.""-'<--`.___\_<|>_/___.'-->'"".
+ * -----------------------------------------------|-|-:--`--\`.;`\-_-/`;.`/---`-:-|-|
+ * -----------------------------------------------\--\-`-.---\_-__\-/__-_/---.-`-/--/
+ * ---------------------------------------======`-.____`-.___\_____/___.-`____.-'======
+ * -------------------------------------------------------------`=---='
+ * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * -----------------------------------------------------佛祖保佑--------永无BUG
+ */
+public class ChannelInboundHandlerAdapter extends ChannelHandlerAdapter implements ChannelInboundHandler {
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    //通道就绪事件
+    @Override
+    public void channelActive(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    //通道读取数据事件
+    @Override
+    public void channelRead(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+}
+
