ff6c1dd17a85eff5fe62ef7de757ceea9d4e658d
guozhizhong <guo_zhizhong@sina.com>
2021-09-27 23:01:15 +0800

    新增

--- netty/src/main/java/com/guozz/netty/chapter6/groupchat/GroupChatServer.java
+++ netty/src/main/java/com/guozz/netty/chapter6/groupchat/GroupChatServer.java
@@ -1,11 +1,16 @@
 package com.guozz.netty.chapter6.groupchat;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * @ClassName GroupChatServer
@@ -58,7 +63,22 @@ public class GroupChatServer {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 128);
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //获取到pipeline
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //向pipeline加入解码器
+                            pipeline.addLast("decoder", new StringDecoder());
+                            //向pipeline加入编码器
+                            pipeline.addLast("encoder", new StringEncoder());
+                            //加入自己的业务处理handler
+                            pipeline.addLast(new GroupChatServerHandler());
+
+                        }
+                    });
 
 
         }finally {
