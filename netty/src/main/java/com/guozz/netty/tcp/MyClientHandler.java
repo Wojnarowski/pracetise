4ea661c080e4db8ed20f2d1a3b7e5f1f57fa980b
guozhizhong <guo_zhizhong@sina.com>
2021-10-23 23:38:10 +0800

    新增

---
+++ netty/src/main/java/com/guozz/netty/tcp/MyClientHandler.java
@@ -0,0 +1,68 @@
+package com.guozz.netty.tcp;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.Charset;
+
+/**
+ * @ClassName MyClientHandler
+ * @Description TODO
+ * @Author paul
+ * @Date 2021/10/23 23:31
+ * Vertion 1.0
+ * -------------------------------------------------------------_ooOoo_
+ * ------------------------------------------------------------o8888888o
+ * ------------------------------------------------------------88"-.-"88
+ * ------------------------------------------------------------(|--_--|)
+ * ------------------------------------------------------------O\--=--/O
+ * ---------------------------------------------------------____/`---'\____
+ * -------------------------------------------------------.'--\\|-----|//--`.
+ * ------------------------------------------------------/--\\|||--:--|||//--\
+ * -----------------------------------------------------/--_|||||--:--|||||---\
+ * -----------------------------------------------------|---|-\\\-----///-|---|
+ * -----------------------------------------------------|-\_|--''\---/''--|---|
+ * -----------------------------------------------------\--.-\__--`-`--___/-.-/
+ * ---------------------------------------------------___`.-.'--/--.--\--`.-.-__
+ * ------------------------------------------------.""-'<--`.___\_<|>_/___.'-->'"".
+ * -----------------------------------------------|-|-:--`--\`.;`\-_-/`;.`/---`-:-|-|
+ * -----------------------------------------------\--\-`-.---\_-__\-/__-_/---.-`-/--/
+ * ---------------------------------------======`-.____`-.___\_____/___.-`____.-'======
+ * -------------------------------------------------------------`=---='
+ * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * -----------------------------------------------------佛祖保佑--------永无BUG
+ */
+public class MyClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private int count;
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //使用客户端发送10条数据 hello,server 编号
+        for (int i = 0; i < 10; ++i) {
+            ByteBuf buffer = Unpooled.copiedBuffer("hello,server " + i, Charset.forName("utf-8"));
+            ctx.writeAndFlush(buffer);
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        byte[] buffer = new byte[msg.readableBytes()];
+        msg.readBytes(buffer);
+
+        String message = new String(buffer, Charset.forName("utf-8"));
+        System.out.println("客户端接收到消息=" + message);
+        System.out.println("客户端接收消息数量=" + (++this.count));
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
