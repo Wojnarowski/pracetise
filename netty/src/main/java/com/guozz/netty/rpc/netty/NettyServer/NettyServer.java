32821d38785078b6bbdec1585cbfdfbd74e461d5
guozhizhong <guo_zhizhong@sina.com>
2021-10-24 10:30:53 +0800

    新增

---
+++ netty/src/main/java/com/guozz/netty/rpc/netty/NettyServer/NettyServer.java
@@ -0,0 +1,82 @@
+package com.guozz.netty.rpc.netty.NettyServer;
+
+
+import com.guozz.netty.sample.NettyServerHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * @ClassName NettyServer
+ * @Description TODO
+ * @Author paul
+ * @Date 2021/10/24 10:22
+ * Vertion 1.0
+ * -------------------------------------------------------------_ooOoo_
+ * ------------------------------------------------------------o8888888o
+ * ------------------------------------------------------------88"-.-"88
+ * ------------------------------------------------------------(|--_--|)
+ * ------------------------------------------------------------O\--=--/O
+ * ---------------------------------------------------------____/`---'\____
+ * -------------------------------------------------------.'--\\|-----|//--`.
+ * ------------------------------------------------------/--\\|||--:--|||//--\
+ * -----------------------------------------------------/--_|||||--:--|||||---\
+ * -----------------------------------------------------|---|-\\\-----///-|---|
+ * -----------------------------------------------------|-\_|--''\---/''--|---|
+ * -----------------------------------------------------\--.-\__--`-`--___/-.-/
+ * ---------------------------------------------------___`.-.'--/--.--\--`.-.-__
+ * ------------------------------------------------.""-'<--`.___\_<|>_/___.'-->'"".
+ * -----------------------------------------------|-|-:--`--\`.;`\-_-/`;.`/---`-:-|-|
+ * -----------------------------------------------\--\-`-.---\_-__\-/__-_/---.-`-/--/
+ * ---------------------------------------======`-.____`-.___\_____/___.-`____.-'======
+ * -------------------------------------------------------------`=---='
+ * ---------------------------------------^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * -----------------------------------------------------佛祖保佑--------永无BUG
+ */
+public class NettyServer {
+
+    public static void startServer(String hostName, int port) {
+        startServer0(hostName, port);
+    }
+
+    //编写一个方法，完成对NettyServer的初始化和启动
+    private static void startServer0(String hostname, int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            io.netty.bootstrap.ServerBootstrap serverBootstrap = new io.netty.bootstrap.ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                                      @Override
+                                      protected void initChannel(SocketChannel ch) throws Exception {
+                                          ChannelPipeline pipeline = ch.pipeline();
+                                          pipeline.addLast(new StringDecoder());
+                                          pipeline.addLast(new StringEncoder());
+                                          pipeline.addLast(new NettyServerHandler()); //业务处理器
+
+                                      }
+                                  }
+
+                    );
+
+            ChannelFuture channelFuture = serverBootstrap.bind(hostname, port).sync();
+            System.out.println("服务提供方开始提供服务~~");
+            channelFuture.channel().closeFuture().sync();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
